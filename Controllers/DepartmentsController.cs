63cf43c48ccfcb1e9ed4af2b68c8dc860efe81ec
Scott A. Hyslop <sahyslop@gmail.com>
2025-04-15 19:23:00 -0400

    Created Models and HomeController Index View()
    Added Customer, Inventory, and InventoryInvoice classes to Models folder
    Created Index View in HomeController to show all Departments

--- Controllers/DepartmentsController.cs
+++ Controllers/DepartmentsController.cs
@@ -6,7 +6,31 @@ namespace NET_9_Business_App_MVC_CRUD.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            var departments = DepartmentsRepository.GetDepartments();
+            var output = $@"
+               <div class='center'>
+                    <h1>Departments</h1>
+                    <a href='/Departments/Create'>Create New Department</a>
+                    <h1>Departments</h1>
+                    <table class='table'>
+                    <thead>
+                      <tr>
+                        <th>Department ID</th>
+                        <th>Department Name</th>
+                        <th>Department Location</th>
+                       </tr>
+                   </thead>
+                    {
+                        string.Join("", departments.Select(dep => $@"<tr>
+                            <td>{dep.DepartmentId}</td>
+                            <td>{dep.DepartmentName}</td>
+                            <td>{dep.DepartmentLocation}</td>
+                            <td><a href='/Departments/Edit/{dep.DepartmentId}'>Edit</a></td>
+                            <td><a href='/Departments/Delete/{dep.DepartmentId}'>Delete</a></td></tr>"))
+                      }
+                   <tbody>
+                </div>";
+            return Content(output, "text/html");
         }
     }
 }
