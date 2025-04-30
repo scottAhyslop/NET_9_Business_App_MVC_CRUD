c7ee24d0f288ec9a710840f911361cf66bb59844
Scott A. Hyslop <sahyslop@gmail.com>
2025-04-30 19:11:28 -0400

    Created ViewComponent for DepartmentsController Index View
    Created Components folder in Shared folder inside of Views
    Created a DepartmentViewComponent ViewComponent class
    Created Invoke () method within DepartmentViewComponent to retrieve a list of Departments from Departments Repository
    Created DepartmentList.cshtml ViewComponent inside DepartmentListView folder inside Components folder
    Copied over code from inside _DepartmentList Partial View into DepartmentListView ViewComponent file
    Invoked DepartmentList ViewComponent within Departments Index View
    Tested and confirmed functional in browser and Postman

---
+++ Views/Shared/Components/DepartmentList/Default.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NET_9_Business_App_MVC_CRUD.Views.Shared.Components.DepartmentList
+{
+    public class _LayoutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Views/Shared/Components/DepartmentList/DepartmentViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NET_9_Business_App_MVC_CRUD.Controllers;
+
+namespace NET_9_Business_App_MVC_CRUD.Views.Shared.Components.DepartmentList
+{
+    public class DepartmentListViewComponent : ViewComponent
+    {
+        
+        public IViewComponentResult Invoke()
+        {
+            var departments = DepartmentsRepository.GetDepartments();
+            return View(departments);
+        }
+
+    }
+}
