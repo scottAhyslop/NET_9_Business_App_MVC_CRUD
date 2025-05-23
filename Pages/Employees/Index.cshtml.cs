33fe075f55bb19b6b4e05e2d0d2bc94779c2878c
Scott A. Hyslop <sahyslop@gmail.com>
2025-05-23 13:33:57 -0400

    Search componentized and implemented for Employees Page
    Implemented Search bar functionality to scripts section of Index.cshtml in the Employees Page by utilizing already existing js script from Departments
    Removed Get functionality from Page Model of Index.cshtml.cs as it was now being populated by GetEmployeesByFilter(string filter) function in the Index.cshtml.cs Page Model
    Better centralized code in _ViewImports
    Tested and confirmed functional, as in Departments, by browser using any search item,  as little as single character

--- Pages/Employees/Index.cshtml.cs
+++ Pages/Employees/Index.cshtml.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using NET_9_Business_App_MVC.CRUD.Models;
 using NET_9_Business_App_MVC_CRUD.Models;
@@ -9,7 +10,13 @@ namespace NET_9_Business_App_MVC_CRUD.Pages.Employees
        public List<Employee>? Employees { get; set; }
         public void OnGet()
         {
-            this.Employees = EmployeesRepository.GetEmployees().ToList();
+            //old method of populating list, now done by js filter
+            //this.Employees = EmployeesRepository.GetEmployees().ToList();
+        }
+
+        public IActionResult OnGetEmployeesByFilter(string? filter)
+        {
+            return ViewComponent("EmployeeList", new { filter });
         }
     }
 }
